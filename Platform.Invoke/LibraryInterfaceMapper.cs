520a7a3e4e53b82ac15cf257d38ac67f2d8ce9f4
hem <henning.moe@hotmail.com>
2014-01-19 15:32:00 +0100

    Moved classes to own files.

--- Platform.Invoke/LibraryInterfaceMapper.cs
+++ Platform.Invoke/LibraryInterfaceMapper.cs
@@ -8,38 +8,6 @@ using System.Threading.Tasks;
 
 namespace Platform.Invoke
 {
-    public interface IMethodCallWrapper
-    {
-        void GenerateInvocation(ILGenerator generator, MethodInfo method, IEnumerable<FieldBuilder> fieldBuilders, bool emitReturn = true);
-    }
-
-    public class MethodCallWrapper : IMethodCallWrapper
-    {
-        private readonly Func<MethodInfo, string> methodToFieldNameMapper;
-
-        public MethodCallWrapper(Func<MethodInfo, string> methodToFieldNameMapper)
-        {
-            this.methodToFieldNameMapper = methodToFieldNameMapper;
-        }
-
-        
-
-        public void GenerateInvocation(ILGenerator generator, MethodInfo method, IEnumerable<FieldBuilder> fieldBuilders, bool emitReturn = true)
-        {
-            var field = fieldBuilders.First(f => f.Name == methodToFieldNameMapper(method));
-            generator.Emit(OpCodes.Ldarg_0); //  this
-            generator.Emit(OpCodes.Ldfld, field); // MethodNameProc _glMethodName. Initialized by constructor.
-            foreach (var item in method.GetParameters().Select((p, i) => new { Type = p, Index = i }))
-            {
-                generator.Emit(OpCodes.Ldarg, item.Index + 1);
-            }
-            generator.EmitCall(OpCodes.Callvirt, field.FieldType.GetMethod("Invoke"), null);
-            if (emitReturn)
-                generator.Emit(OpCodes.Ret);
-        }    
-    }
-
-
     public class LibraryInterfaceMapper
     {
         private readonly AssemblyBuilder assemblyBuilder;
